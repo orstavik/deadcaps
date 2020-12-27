faa9de0efbd04450d539e3efcbd106721efd3ac7
orstavik <orstavik77@gmail.com>
2020-12-27 12:43:47 +0100

    adding deadless and refactoring script

--- linux2/deadcaps.py
+++ linux2/deadcaps.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-#
-#   NORMAL (no caps) state.
-#     -> CAPSLOCK KEYDOWN, NO event, go to DEADCAPS1 state.
-#     -> ANY KEY, send event.
-#   DEADCAPS1 state (single key caps activated).
-#     -> CAPSLOCK KEYUP, NO event, go to DEADCAPS2 state.
-#     -> ANY KEY, send event, and go to NORMAL state.
-#   DEADCAPS2 state (single key caps).
-#     -> CAPSLOCK KEYDOWN, send event, and go to CAPSLOCK1 state.
-#     -> ANY KEY, send event, and go to NORMAL state.
-#   CAPSLOCK1 state (multi key caps).
-#     -> CAPSLOCK KEYUP, send event, and go to CAPSLOCK2 state.
-#     -> ANY KEY, send event.
-#   CAPSLOCK2 state (multi key caps).
-#     -> CAPSLOCK KEYUP, send event, and go to NORMAL state.
-#     -> ANY KEY, send event.
+# CAPS (auto revert after one other keydown or CAPS to CAPSLOCK)
+# CAPSLOCK (turn off when CAPS) (this is done by the system).
+# SEMI (auto revert after one other keydown)
+# LESSLOCK (turn off when space or LESS)
 
 
 import sys
@@ -23,64 +11,58 @@ from libevdev import InputEvent
 
 
 def debug(msg, e, caps_state):
-    print("{}. CAPS_STATE: {}. Event type: {:02x} {} code {:03x} {:12s} value{:1d}".format(msg, caps_state, e.type.value, e.type.name, e.code.value, e.code.name, e.value))   
+    print("{}. CAPS_STATE: {}. Event type: {:02x} {} code {:03x} {:12s} value{:1d}".format(msg, caps_state, e.type.value, e.type.name, e.code.value, e.code.name, e.value))
 
 
-def deadCapsIt(e, wrap):
-    return [InputEvent(wrap, 1), e, InputEvent(wrap, 0), InputEvent(libevdev.EV_SYN.SYN_REPORT, 0)]
+def wrapModifierKeys(list, ctrl, shift, alt):
+    if shift:
+        list.insert(0, InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT,1))
+        list.append(InputEvent(libevdev.EV_KEY.KEY_LEFTSHIFT,0))
+    if alt:
+        list.insert(0, InputEvent(libevdev.EV_KEY.KEY_RIGHTALT,1))
+        list.append(InputEvent(libevdev.EV_KEY.KEY_RIGHTALT,0))
+    if ctrl:
+        list.insert(0, InputEvent(libevdev.EV_KEY.KEY_LEFTCTRL,1))
+        list.append(InputEvent(libevdev.EV_KEY.KEY_LEFTCTRL,0))
+    return list
 
+def deadCapsIt(e, shift, alt):
+    list = wrapModifierKeys([e], False, shift, alt)
+    list.append(InputEvent(libevdev.EV_SYN.SYN_REPORT,0))
+    return list
 
-# lockKey is true if the key is the lockKey, 
-# keydown: KEYDOWN => True, KEYUP => False
-def statemachine(state, lockKey, keydown):
-    #normal (no lock) state
-    if state == 0 :
-        if lockKey and keydown :
-            return 10
-        return 0
-    #toggle1 state
-    if state == 10 :
-        if lockKey and not keydown :
-            return 11
-        return 0
-    #toggle2 state
-    if state == 11 :
-        if lockKey and keydown :
-            return 20
-        return 0
-    #lock1 state
-    if state == 20 :
-        if lockKey and not keydown :
-            return 21
-        return 20
-    #lock2 state
-    if state == 21 :
-        if lockKey and not keydown :
-            return 0
-        return 21
+arrows = {
+    'KEY_A': libevdev.EV_KEY.KEY_UP,
+    'KEY_S': libevdev.EV_KEY.KEY_DOWN,
+    'KEY_D': libevdev.EV_KEY.KEY_LEFT,
+    'KEY_F': libevdev.EV_KEY.KEY_RIGHT,
+    'KEY_Z': libevdev.EV_KEY.KEY_Z,
+    'KEY_X': libevdev.EV_KEY.KEY_X,
+    'KEY_C': libevdev.EV_KEY.KEY_C,
+    'KEY_V': libevdev.EV_KEY.KEY_V,
+    'KEY_B': libevdev.EV_KEY.KEY_A,
+    'KEY_E': libevdev.EV_KEY.KEY_BACKSPACE,
+    'KEY_R': libevdev.EV_KEY.KEY_DELETE
+}
+homePageArrows = {
+    'KEY_A': libevdev.EV_KEY.KEY_PAGEUP,
+    'KEY_S': libevdev.EV_KEY.KEY_PAGEDOWN,
+    'KEY_D': libevdev.EV_KEY.KEY_HOME,
+    'KEY_F': libevdev.EV_KEY.KEY_END,
+}
 
-
-# NORMAL state
-#   KEYDOWN lock, NO event, ACTIVE
-#   ALL OTHER KEYDOWN/UP, send event through, NORMAL STATE
-# ACTIVE
-#   KEYUP lock, NO event, ACTIVE
-#   ALL OTHER KEYDOWN/UP, wrap event, NORMAL STATE
-
-# lockKey is true if the key is the lockKey, 
-# keydown: KEYDOWN => True, KEYUP => False
-def smallStatemachine(state, lockKey, keydown):
-    #normal (no lock) state
-    if state == 0 :
-        if lockKey and keydown :
-            return 1
-        return 0
-    #active state
-    if state == 1 :
-        if lockKey and not keydown :
-            return 1
-        return 0
-
+def deadlessKey(key, homePageMode):
+    if homePageMode and key in homePageArrows:
+        return homePageArrows[key]
+    if key in arrows:
+        return arrows[key]
+    return null 
+    
+def deadlessIt(key, ctrl, shift):
+    list = wrapModifierKeys([InputEvent(key, 1)], ctrl, shift, False)
+    list.append(InputEvent(key,0))
+    list.append(InputEvent(libevdev.EV_SYN.SYN_REPORT,0))
+    return list
 
 def main(args):
     path = args[1]
@@ -97,40 +79,92 @@ def main(args):
     uidev = d.create_uinput_device()
     print('Device is at {}'.format(uidev.devnode))
 
+    state = 'NORMAL'   #NORMAL/DEADCAPS/CAPSLOCK/SEMIDEAD/DEADLESS
+    lessJ = False
+    lessK = False
+    lessL = False
+
     while True:
         for e in d.events():
-            
+
+            # debug('keyboard: ', e, state) 
+
             # ignore non-keypress events
             if e.type.name != 'EV_KEY' :
                 uidev.send_events([e])
                 continue
 
-            is_caps = e.code.name == 'KEY_CAPSLOCK'
-            old_caps_state = caps_state
-            caps_state = statemachine(caps_state, is_caps, e.value)
-            # a1) if lockkey and new state is 10 or 11, then no event.
-            if is_caps and caps_state >= 10 and caps_state <= 11 :
+            # debug('keydown/keyup: ', e, state) 
+
+            key = e.code.name
+            ## keyup handlers
+            if not e.value:
+                # handling JKL modifier keys in navigation mode
+                if state == 'DEADLESS':
+                    if key == 'KEY_J':
+                        lessJ = False
+                    elif key == 'KEY_K':
+                        lessK = False
+                    elif key == 'KEY_L':
+                        lessL = False
+                    continue
+
+                uidev.send_events([e])
                 continue
-            # b1) if old state is 10 or 11 and new state is 0, then deadcaps around event.
-            elif caps_state == 0 and old_caps_state >= 10 and old_caps_state <= 11 :
-                uidev.send_events(deadCapsIt(e, libevdev.EV_KEY.KEY_LEFTSHIFT))
+
+            # debug('keydown: ', e, state)
+
+            ## keydown handlers
+            #default state. intercept deadkeys, otherwise do nothing.
+            if state == 'NORMAL':
+                if key == 'KEY_CAPSLOCK':
+                    state = 'DEADCAPS'
+                elif key == 'KEY_SEMICOLON':
+                    state = 'SEMIDEAD'
+                elif key == 'KEY_102ND':          # < less than key
+                    state = 'DEADLESS'
+                else:
+                    uidev.send_events([e])
                 continue
-                
-            is_pinky = e.code.name == 'KEY_SEMICOLON'
-            old_pinky_state = pinky_state
-            pinky_state = smallStatemachine(pinky_state, is_pinky, e.value)
-            # a2) pinkystate is being set to active, then don't send the key event
-            if pinky_state == 1 :
+
+            if state == 'CAPSLOCK':
+                if key == 'KEY_CAPSLOCK':
+                    state = 'NORMAL'
+                uidev.send_events([e])
                 continue
-            # b2) if old state is 10 or 11 and new state is 0, then deadcaps around event.
-            elif pinky_state == 0 and old_pinky_state == 1 :
-                uidev.send_events(deadCapsIt(e, libevdev.EV_KEY.KEY_RIGHTALT))
+
+            if state == 'DEADCAPS':
+                if key == 'KEY_CAPSLOCK':
+                    state = 'CAPSLOCK'
+                    uidev.send_events([e])
+                else :
+                    state = 'NORMAL'
+                    uidev.send_events(deadCapsIt(e, True, False))
                 continue
             
-            # c) else, then pass the event as is.
-            uidev.send_events([e])
+            if key == 'KEY_CAPSLOCK':       # caps key doesnt work in semidead or deadless state
+                continue
+            
+            if state == 'SEMIDEAD':
+                state = 'NORMAL'
+                uidev.send_events(deadCapsIt(e, False, True))
+                continue
+            
+            if state == 'DEADLESS':
+                if key == 'KEY_102ND' or key == 'KEY_SPACE':
+                    state = 'NORMAL'
+                elif key == 'KEY_J':
+                    lessJ = True
+                elif key == 'KEY_K':
+                    lessK = True
+                elif key == 'KEY_L':
+                    lessL = True
+                else:
+                    key = deadlessKey(key, lessL)
+                    if key:
+                        uidev.send_events(deadlessIt(key, lessJ, lessK))
+                continue
 
-                
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: {} /dev/input/eventX".format(sys.argv[0]))
